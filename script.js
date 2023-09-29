63ff9e47fa2ab69d1ad034005cff28a5bba7b393
Righteous1ice <hoplabradley@gmail.com>
2023-09-29 19:29:07 +0100

    Fixing the create PLayer function

--- script.js
+++ script.js
@@ -6,44 +6,100 @@ const resetBtn = document.getElementById('reset-btn');
 const backdrop = document.querySelector('.backdrop');
 const playerOneDisplay = document.querySelector('.player-one');
 const playerTwoDisplay = document.querySelector('.player-two');
+const playerOneInput = document.getElementById('player-one-input');
+const playerTwoInput = document.getElementById('player-two-input');
+const playerSubmitBtn = document.getElementById('submit-btn');
 
 
-const craetePlayers = (name, marker,) => {
-    const sayName = () => 
-    playerOneDisplay.innerHTML = `${name} is ${marker}`
-    playerTwoDisplay.innerHTML = `${name} is ${marker}`;
-    
-    return {name, marker, sayName};
+
+
+const createPlayers = (name, marker) => {
+    let playerName = name;
+    let playerMarker = marker;
+
+    getName = () => playerName;
+    getMarker = () => playerMarker;
+
+    return {
+        getName,
+        getMarker
+    };
 };
 
-const playGame = (name, marker, )
 
-const playerOne = craetePlayers('Bradley', 'X');
-const playerTwo = craetePlayers('Tessa', 'O');
-playerOne.sayName();
 
+
+//const playerOne = createPlayers('Bradley', 'X');
+//const playerTwo = createPlayers('Tessa', 'O')
+
+
+
+
+
+
+playerSubmitBtn.addEventListener('click', () => {
+    const playerOneName = playerOneInput.value;
+    const playerTwoName = playerTwoInput.value;
+
+    const playerOne = createPlayers(playerOneName, 'X');
+    const playerTwo = createPlayers(playerTwoName, 'O');
+  
+    playerOneDisplay.innerHTML = `${playerOne.getName()} is ${playerOne.getMarker()}`
+    playerTwoDisplay.innerHTML = `${playerTwo.getName()} is ${playerTwo.getMarker()}`
+    
+
+});
+
+
+
+const togglePlayer = (currentPlayer) => {
+    return currentPlayer === 'X' ? 'O' : 'X';
+}
+
+let  currentPlayer = 'X';
 
 
 
 
 cells.forEach(cell => {
     cell.addEventListener('click', () => {
-        if(playerOne.marker === 'X'){
-            cell.classList.add('x');
-
-        }else {
-            cell.classList.add('circle');
+        if(currentPlayer === 'X'){
+            cell.classList.add('currentPlayer')
         }
+
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X'
+        console.log('hel')
     })
 })
+
+
+
+
+
+
+
+
+
+//cells.forEach(cell => {
+//    cell.addEventListener('click', () => {
+//        if(playerOne.marker === 'X'){
+//            cell.classList.add('x');
+//
+//        }else {
+//            cell.classList.add('circle');
+//        }
+//    })
+//})
+/*
 
 if(playerOne.name === 'Bradley'){
     winnerModal.classList.add('active');
     backdrop.classList.add('active');
-    winnerNameDisplay.innerHTML = `${playerTwo.name}, is the Winner!`
+    winnerNameDisplay.innerHTML = `${playerOne.name}, is the Winner!`
 }
 
 resetBtn.addEventListener('click', () => {
     winnerModal.classList.remove('active');
     backdrop.classList.remove('active');
 })
+*/
