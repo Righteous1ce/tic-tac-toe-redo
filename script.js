3f98cf83b4efe00fe3601c4c101499732b088c89
Righteous1ice <hoplabradley@gmail.com>
2023-09-30 20:09:26 +0100

    winner check works but needs user input first

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const cells = document.querySelectorAll('.cells');
 const winnerModal = document.querySelector('.winner-modal');
 const winnerNameDisplay = document.querySelector('.winner-name-display');
@@ -9,7 +8,6 @@ const playerTwoDisplay = document.querySelector('.player-two');
 const playerOneInput = document.getElementById('player-one-input');
 const playerTwoInput = document.getElementById('player-two-input');
 const playerSubmitBtn = document.getElementById('submit-btn');
-
 
 
 
@@ -28,15 +26,9 @@ const createPlayers = (name, marker) => {
 
 
 
-
-//const playerOne = createPlayers('Bradley', 'X');
-//const playerTwo = createPlayers('Tessa', 'O');
-
-
 let playerOne = '';
 let playerTwo = '';
-
-let currentPlayer = 'X';
+let currentPlayer = 'X'; 
 
 playerSubmitBtn.addEventListener('click', () => {
     const playerOneName = playerOneInput.value;
@@ -44,51 +36,89 @@ playerSubmitBtn.addEventListener('click', () => {
 
     playerOne = createPlayers(playerOneName, 'X');
     playerTwo = createPlayers(playerTwoName, 'O');
+
   
     playerOneDisplay.innerHTML = `${playerOne.getName()} is ${playerOne.getMarker()}`
     playerTwoDisplay.innerHTML = `${playerTwo.getName()} is ${playerTwo.getMarker()}`
-    
-
 });
 
 
 const togglePlayer = () => {
-
     currentPlayer = currentPlayer === 'X' ? 'O': 'X';
-    
 };
 
-//console.log('current player ' + currentPlayer)
-//togglePlayer();
-//console.log('current player ' + currentPlayer)
-//togglePlayer();
-//console.log('current player ' + currentPlayer)
 
 
 cells.forEach(cell => {
     cell.addEventListener('click', () => {
         if(currentPlayer === 'X'){
-            cell.classList.add('x');
-            togglePlayer();
-            
+            cell.classList.add('x'); 
         }else {
             cell.classList.add('circle');
-            togglePlayer();
-            
         }
+
+        togglePlayer();
+        
+        const winner = checkForWinner();
+        if (winner) {
+            winnerModal.classList.add('active');
+            backdrop.classList.add('active');
+            winnerNameDisplay.innerHTML = `${winner}, is the Winner!`
+            
+        } else{
+            console.log('no winner yet');
+        }
+
+
     });
 });
 
 
-/*
-if(playerOne.name === 'Bradley'){
-    winnerModal.classList.add('active');
-    backdrop.classList.add('active');
-    winnerNameDisplay.innerHTML = `${playerOne.name}, is the Winner!`
+
+
+function checkForWinner() {
+    const winningCells = document.querySelectorAll('.cells');
+
+    winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for(let combinations of winningCombinations){
+        const [a, b, c] = combinations
+        if(
+            winningCells[a].classList.contains('x')&&
+            winningCells[b].classList.contains('x') &&
+            winningCells[c].classList.contains('x')
+        ){
+            return playerOne.getName();
+        } else if(
+            winningCells[a].classList.contains('circle')&&
+            winningCells[b].classList.contains('circle')&&
+            winningCells[c].classList.contains('circle')
+        ){
+            return playerTwo.getName();
+        }
+    }
+    return false
 }
+
+
+
+
 
 resetBtn.addEventListener('click', () => {
     winnerModal.classList.remove('active');
     backdrop.classList.remove('active');
-})
-*/
+    playerOneInput.value = '';
+    playerTwoInput.value = '';
+    playerOneDisplay.innerHTML = '';
+    playerTwoDisplay.innerHTML = '';
+    
+    
+});
